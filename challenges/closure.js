b95bdff2df5969f308569cf61823fd50f65308d1
MelissaTanksley <melissa.tanksley@gmail.com>
2020-08-21 08:53:48 -0700

    MelissaTanksley2

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,16 @@ myFunction();
 
 // Explanation: 
 
+//Because of closure, all variables in a parent function can be accessed by its child (nested function).
+
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+
+function mySummation(num) {
+  return 1 + 2 + 3 + 4 + 5; // 15
+  }
+console.log (mySummation);
+  
